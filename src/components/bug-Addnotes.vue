2706527bf1431d31f4f652c678fabfb58ad7fe13
ben <bholsinger@gmail.com>
2019-06-09 15:51:04 -0600

    added comments for templates

--- src/components/bug-Addnotes.vue
+++ src/components/bug-Addnotes.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
+    <!--one row 3 columns  user  message    addNote-->
+    <!-- add note should handle the submit of the form for the selected bug-->
+
+    <!--one row 3 columns for the details    delete button-->
+
 
 
   </div>
 
 </template>
+
+<!-- var note = new Schema({
+content: { type: String, required: true },
+bug: { type: ObjectId, ref: 'Bug' required: true },
+creator: { type: String, required: true } //The provided name for who made the note
+user: { type: String, required: true },
+flagged: { type: String, enum: ["pending", "completed", "rejected"] }
+}, { timestamps: true }) -->
 
 
 <script>
--- src/views/ReportDetails.vue
+++ src/views/ReportDetails.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="report-list">
     <h1>List of bugs below</h1>
+    <!-- there should be the selected bug details here-->
+    <!-- there needs to be a form with the description of the bug
+          the description should be editable and added as a note -->
+    <!-- below the selected bug details needs to be a close button to change 
+         the status to closed  -->
     <reportList />
+    <!-- here needs to be where we can add notes-->
+    <bugAddNotes />
+
   </div>
 
 </template>
