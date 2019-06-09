afd539c0229ab756bb1774fdf58caaf706cc922d
bholsinger09 <bholsinger@gmail.com>
2019-06-09 17:44:38 -0600

    added state to store and actions

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
-import { SSL_OP_NETSCAPE_CA_DN_BUG } from 'constants';
+
 
 Vue.use(Vuex)
 
@@ -13,7 +13,8 @@ let _api = axios.create({
 export default new Vuex.Store({
   state: {
     bugs: [],
-    bug:{}
+    bug:{},
+    notes:{}
 
   },
   mutations: {
@@ -24,12 +25,26 @@ export default new Vuex.Store({
     },
     setBug(state, id) {
       state.bug = id
+    },
+    //set notes is where the details of the bug get edited 
+    setNotes(state,results){
+      state.notes = results
     }
     
     
 
   },
   actions: {
+
+    //vuex note
+    //actions use commit to call on mutations
+    //note they are done once the promise has gotten the data
+
+    //dispatch is used on asyc requests such as calling on another action
+    //all actions are called from components 
+
+    //get request below: 
+
     async getBugs({ commit}) {
       try {
         let res = await _api.get('bugs')
@@ -43,9 +58,22 @@ export default new Vuex.Store({
       }
     },
     
-    async getBug({ commit}, id) {
+    async getBugById({ commit}, id) {
       try {
-        let res = await _api.get('id')
+        let res = await _api.get('bugs/:id')
+        console.log(res.data.results)
+        commit('setBugs', res.data.results)
+        //commit calls to mutations  when promise is finished
+
+      } catch (error) {
+        console.error(error)
+
+      }
+    },
+
+    async getBugByIdNotes({ commit}, id) {
+      try {
+        let res = await _api.get('bugs/:id/notes')
         console.log(res.data.results)
         commit('setBugs', res.data.results)
         //commit calls to mutations 
@@ -56,16 +84,69 @@ export default new Vuex.Store({
       }
     },
 
-    
 
-    createPost({ dispatch }, payload) {
-      _api.post('bugs', payload)
-        .then(res => {
-          dispatch('getBugs')
-          //dispatch actions when called to $store
-        })
-        .catch(err => console.error(err))
+
+    //post request below: 
+    //payload is data pasted from components
+
+     async createPost({ dispatch }, payload) {
+       try {
+         let res = await _api.post('bugs')
+         dispatch('getBugs')
+         
+       } catch (error) {
+         console.error(error)
+         
+       }
+      
     },
+
+    async createNotesbyId({ dispatch }, payload) {
+      try {
+        let res = await _api.post('bugs/:id/notes')
+        dispatch('getBugs')
+        
+      } catch (error) {
+        console.error(error)
+        
+      }
+     
+   },
+
+   async createPost({ dispatch }, payload) {
+    try {
+      let res = await _api.post('bugs')
+      dispatch('getBugs')
+      
+    } catch (error) {
+      console.error(error)
+      
+    }
+   
+ },
+
+ async editPost({ commit}, payload) {
+  try {
+    let res = await _api.put('bugs/'+ payload.id, payload)
+    commit('setNotes', res.data.results)
+    
+  } catch (error) {
+    console.error(error)
+    
+  }
+ 
+},
+
+
+
+
+
+
+
+
+
+
+
   }
 
 })
